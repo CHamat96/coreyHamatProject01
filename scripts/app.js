d06170e9b0a8c5d432553f1243f1596e8d6193bf
Corey <coreyhamat@gmail.com>
2021-09-13 16:51:51 -0400

    reorganized the code structure

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,4 @@
 // DOM Manipulation: add user comments to blog.html page
-// Event Listener: Form submitted
-  // Get value of "#name" input as well as the 'textarea' value
-  // create a 'commentFlex' div that holds 
 const app = {}
 
 // Create variables for html tags/elements
@@ -10,14 +7,25 @@ const commentList = document.querySelector('.postComments')
 // Event Listener: When user Submits the comment form, log data from 'name' and 'textarea' inputs
 commentForm.addEventListener('submit', function(event) {
   event.preventDefault();
+
+  // log the user's 'name' input into a variable
   const nameInput = commentForm.querySelector('#name');
   const userName = nameInput.value;
+
+  // Log the user's 'comment' into a variable
   const userComment = commentForm.querySelector('textarea');
   const comment = userComment.value;
-  app.postComment(userName, comment)
+
+  // if there is content in the user's input, kickstart the postComment function
+  if(userName, comment) {
+    app.postComment(userName, comment)
+  }
+
+
 })
 
 app.postComment = (name, comment) => {
+  // Log the comment date into a variable
   const postDay = new Date();
   const dateString = postDay.toDateString();
 
@@ -25,30 +33,41 @@ app.postComment = (name, comment) => {
   const commentContainer = document.createElement('div');
   commentContainer.classList.add('commentFlex');
 
+  // create a new IMG container for user profile photo
+  const imgContainer = document.createElement('div')
+  imgContainer.classList.add('profileContainer')
+
+  // add a placeholder image to store within the imgContainer
+  const userImage = document.createElement('img')
+  userImage.src = `https://picsum.photos/100`
+  userImage.alt = `photo of ${name}`
+
+  // add the new <img> to the imgContainer
+  imgContainer.appendChild(userImage)
+
+  // Create a <div> to store the comment heading + content
+  const userComment = document.createElement('div');
+  userComment.classList.add('commentContent')
   // Create a heading with the date + time of comment
   const commentHeading = document.createElement('h4');
   commentHeading.classList.add('commentDate');
   commentHeading.innerText = `${dateString} by ${name}`;
-
-  // create a new IMG container for user profile photo (using placeholder image)
-  const imgContainer = document.createElement('div')
-  imgContainer.classList.add('profileContainer')
-  const userImage = document.createElement('img')
-  userImage.src = `https://picsum.photos/100`
-  userImage.alt = `photo of ${name}`
-  imgContainer.appendChild(userImage)
-
-  const userComment = document.createElement('div');
-  userComment.classList.add('commentContent')
+  
+  // Create a new <p> element to store the user's comment
   const commentContent = document.createElement('p');
   commentContent.innerText = comment
+
+  // Append the heading & user's comment to the to the parent <div> container
   userComment.appendChild(commentHeading)
   userComment.appendChild(commentContent)
 
-  commentList.appendChild(commentContainer);
-
+  // append all comment content into newly-created Comment Flex Container
   commentContainer.appendChild(imgContainer);
   commentContainer.appendChild(userComment);
+
+
+  // append the new comment Flex Container to the comment list
+  commentList.appendChild(commentContainer);
 
 }
 
